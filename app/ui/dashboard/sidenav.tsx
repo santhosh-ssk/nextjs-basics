b65b7f2e5658c02e0b5424cbbce676c0ce9ea74d
Santhosh Kumar <santhoshKumar.ssk3222@gmail.com>
2024-09-05 22:13:58 +0530

    Completed the nextjs basics lessons

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,9 +1,16 @@
+// "use client";
+
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import { useEffect } from 'react';
 
 export default function SideNav() {
+  // useEffect(() => {
+  //   console.log("sidebar renderer");
+
+  // });
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
